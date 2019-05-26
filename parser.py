20c8d2ff638ec3886f181cd9069a0b080605f304
AntonyDamico <antonydamico29@gmail.com>
2019-05-26 15:48:45 -0400

    some conditions

--- parser.py
+++ parser.py
@@ -19,9 +19,9 @@ def p_rec_statement(p):
     '''S : statement S
          | statement END_LINE S'''
     if len(p) == 3:
-        p[0] = ('statement', [p[1], p[2]])
+        p[0] = ('statement', p[1], p[2])
     else:
-        p[0] = ('statement', [p[1], p[3]])
+        p[0] = ('statement', p[1], p[3])
 
 
 def p_statement(p):
@@ -33,3 +33,18 @@ def p_declarations(p):
     'statement : declarations'
     p[0] = p[1]
 
+
+# declarations
+
+def p_variables(p):
+    'declarations : VAR ids'
+    p[0] = ('VAR', p[2])
+
+
+def p_write(p):
+    'declarations : WRITE LPAREN expressions RPAREN'
+    p[0] = ('WRITE', p[3])
+
+def p_if(p):
+    'declaration : IF LPAREN condition RPAREN return_dec'
+    p[0] = ('IF', p[3], p[5])
